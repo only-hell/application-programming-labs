fb73efc83145c96bf7e19d213bcd1bb133ef69f9
only-hell <91896993+only-hell@users.noreply.github.com>
2024-11-09 15:44:39 +0400

    Update iterator.py

--- lab_2/iterator.py
+++ lab_2/iterator.py
@@ -3,25 +3,41 @@ import csv
 
 class ImageIterator:
     def __init__(self, csv_path: str) -> None:
-        self.csv_path = csv_path  # сохраняем путь к файлу
-        self.path_list = self.__load_csv()  # пути из CSV
+        '''
+
+        :param csv_path:
+        '''
+        self.csv_path = csv_path
+        self.path_list = self.__load_csv()
         self.limit = len(self.path_list)
         self.counter = 0
 
     def __iter__(self) -> "ImageIterator":
+        '''
+
+        :return:
+        '''
         return self
 
     def __next__(self) -> str:
+        '''
+
+        :return:
+        '''
         if self.counter < self.limit:
             next_element = self.path_list[self.counter]
             self.counter += 1
-            return next_element  # возвращаем путь
+            return next_element
         else:
             raise StopIteration("Итерация прервалась")
 
     def __load_csv(self) -> list:
+        '''
+
+        :return:
+        '''
         with open(self.csv_path, mode='r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
-            next(reader)  # пропускаем заголовок
-            path_list = list(row[1] for row in reader)  # извлекаем пути из второго столбца каждой строки
-            return path_list  # возвращаем список путей
+            next(reader)
+            path_list = list(row[1] for row in reader)
+            return path_list
