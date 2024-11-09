8f444756cab96fc3ca34b444b014ab1916d279c0
only-hell <91896993+only-hell@users.noreply.github.com>
2024-11-09 15:41:05 +0400

    Update annotation.py

--- lab_2/annotation.py
+++ lab_2/annotation.py
@@ -3,6 +3,12 @@ import os
 
 
 def create_annotation(image_directory: str, csv_filepath: str) -> None:
+    '''
+
+    :param image_directory:
+    :param csv_filepath:
+    :return:
+    '''
     try:
         if not os.path.exists(image_directory):
             raise FileNotFoundError(f"Директория с изображениями не найдена: {image_directory}")
@@ -10,15 +16,12 @@ def create_annotation(image_directory: str, csv_filepath: str) -> None:
             writer = csv.writer(csvfile)
             writer.writerow(["Relative path", "Absolute path"])
             for filename in os.listdir(image_directory):
-                # Проверяем, является ли текущий элемент файлом (а не директорией)
                 if os.path.isfile(os.path.join(image_directory, filename)):
-                    # Формируем относительный путь к файлу относительно директории, где расположен CSV-файл
                     relative_path = os.path.relpath(
-                        os.path.join(image_directory, filename),  # Полный путь к файлу изображения
+                        os.path.join(image_directory, filename),
                         start=os.path.dirname(csv_filepath)
-                        # Директория CSV-файла как начальная точка для относительного пути
+
                     )
-                    # Формируем абсолютный путь к файлу
                     absolute_path = os.path.abspath(os.path.join(image_directory, filename))
                     writer.writerow([relative_path, absolute_path])
 
